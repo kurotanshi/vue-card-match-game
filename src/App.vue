15e58a3e2cd8aca5e513b84e0c6a051336243100
Chinesebook <swer801219@gmail.com>
2022-12-24 01:17:11 +0800

    feat: extract card

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 //  5. 將卡片獨立抽出為 Card.vue 元件
 
 import { ref } from 'vue';
+import Card from './card.vue'
 
 const cards = ref([]);
 const openedCard = ref([]);
@@ -38,79 +39,16 @@ const clickHandler = (idx) => {
     </div>
 
     <div class="rounded-xl mx-auto border-4 mt-12 grid grid-flow-col p-10 w-[900px] gap-2 grid-rows-4">
-      
-      <div 
-        v-for="(n, idx) in cards"
-        class="flip-card"
-        :class="{
-          'open': openedCard.includes(idx)
-        }"
-        @click="clickHandler(idx)">
-        <div class="flip-card-inner" v-if="cards[idx] > 0">
-          <div class="flip-card-front"></div>
-          <div class="flip-card-back">
-            <img :src="`./img/cat-0${n}.jpg`" alt="">
-          </div>
-        </div>
-      </div>
-
+      <Card
+        v-for="(card, idx) in cards"
+        :isOpen="openedCard.includes(idx)"
+        :isPicked="false"
+        :imgUrl="`./img/cat-0${card}.jpg`"
+        @click="clickHandler(idx)"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
-.flip-card {
-  display: block;
-  width: 150px;
-  height: 100px;
-  border: 1px solid #f1f1f1;
-  perspective: 1000px;
-  margin: 1rem;
-}
-
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
-}
-
-.flip-card.open .flip-card-inner {
-  transform: rotateY(180deg);
-}
-
-.flip-card-front,
-.flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border: 5px solid #aaa;
-  -webkit-backface-visibility: hidden;
-  /* Safari */
-  backface-visibility: hidden;
-}
-
-.flip-card-front {
-  background-color: #eee;
-  background-image: url('./img/cat-bg.png');
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position-x: 47.5%;
-  color: black;
-}
-
-.flip-card-back {      
-  background-color: #eee;
-  transform: rotateY(180deg);
-}
-
-.flip-card-back img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-  background-color: #eee;
-}
 </style>
